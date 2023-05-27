d9871b364772e4844e3f82e1513bb096b230df30
Yuhang Wang <83380147+TsingYiPainter@users.noreply.github.com>
2023-05-27 23:31:44 +0800

    complex test

--- tests/switching_test/test_network.py
+++ tests/switching_test/test_network.py
@@ -32,8 +32,11 @@ class TriangleTopo(Topo):
         h1 = self.addHost('h1')
         h2 = self.addHost('h2')
         h3 = self.addHost('h3')
+        #print("add s1")
         s1 = self.addSwitch('s1')
+        #print("add s2")
         s2 = self.addSwitch('s2')
+        #print("add s3")
         s3 = self.addSwitch('s3')
         self.addLink(h1, s1)
         self.addLink(h2, s2)
@@ -43,9 +46,48 @@ class TriangleTopo(Topo):
         self.addLink(s3, s1)
 
 
+
+class SixTopo(Topo):
+    def __init__(self, **opts):
+        Topo.__init__(self, **opts)
+        h1 = self.addHost('h1')
+        h2 = self.addHost('h2')
+        h3 = self.addHost('h3')
+        h4 = self.addHost('h4')
+        h5 = self.addHost('h5')
+        h6 = self.addHost('h6')
+        s1 = self.addSwitch('s1')
+        s2 = self.addSwitch('s2')
+        s3 = self.addSwitch('s3')
+        s4 = self.addSwitch('s4')
+        s5 = self.addSwitch('s5')
+        s6 = self.addSwitch('s6')
+        self.addLink(h1, s1)
+        self.addLink(h2, s2)
+        self.addLink(h3, s3)
+        self.addLink(h4, s4)
+        self.addLink(h5, s5)
+        self.addLink(h6, s6)
+
+        self.addLink(s1, s2)
+        self.addLink(s1, s4)
+        self.addLink(s1, s5)
+
+        self.addLink(s2, s6)
+
+        self.addLink(s3, s4)
+        self.addLink(s3, s6)
+
+        self.addLink(s4, s5)
+
+        self.addLink(s5, s6)
+        
+
+
+
 def run_mininet():
     import time
-    topo = TriangleTopo()
+    topo = SixTopo()
     net = Mininet(topo=topo, autoSetMacs=True, controller=RemoteController)
     for h in net.hosts:
         disable_ipv6(h)
